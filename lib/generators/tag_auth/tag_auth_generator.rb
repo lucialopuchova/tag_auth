428719aab53d747d66098e6ef9b56743423ad16c
lucia.lopuchova <lucia.lopuchova@softpoint.sk>
2023-10-07 22:37:44 +0200

    Add tag auth generator

---
+++ lib/generators/tag_auth/tag_auth_generator.rb
@@ -0,0 +1,31 @@
+require 'rails/generators/active_record'
+require 'rails/generators/named_base'
+
+module TagAuth
+  module Generators
+    class TagAuthGenerator < Rails::Generators::NamedBase
+
+      desc 'Generates a migration modifying a table with the given NAME which' \
+           "adds a new column for storing user's tag value."
+
+      def copy_tag_auth_migration
+        migration_template 'add_tag_column_migration.rb.erb', "#{migration_path}/add_devise_to_#{file_name}", migration_version: migration_version
+      end
+
+      def migration_path
+        if Rails.version >= '5.0.3'
+          db_migrate_path
+        else
+          @migration_path ||= File.join('db', 'migrate')
+        end
+      end
+
+      def migration_version
+        return unless Rails::VERSION::MAJOR >= 5
+
+        "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
+      end
+
+    end
+  end
+end
