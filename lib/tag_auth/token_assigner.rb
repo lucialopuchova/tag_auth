8a2163fc000d626d7ced7d84b6678d7cf358d1c3
lucia.lopuchova <lucia.lopuchova@softpoint.sk>
2023-10-29 22:35:46 +0100

    Add TokenAssigner

---
+++ lib/tag_auth/token_assigner.rb
@@ -0,0 +1,30 @@
+require 'devise'
+
+module TagAuth
+  class TokenAssigner
+
+    def initialize(model_instance)
+      @model_instance = model_instance
+    end
+
+    def assign_token
+      token = generate_token
+      @model_instance.update(authentication_token: token)
+
+      token
+    end
+
+    private
+
+    def generate_token
+      loop do
+        token = Devise.friendly_token
+        break token if token_suitable?(token)
+      end
+    end
+
+    def token_suitable?(token)
+      @model_instance.class.where(authentication_token: token).exists?
+    end
+  end
+end
