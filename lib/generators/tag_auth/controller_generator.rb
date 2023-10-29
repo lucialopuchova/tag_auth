c70abbfe3f38ce19e16dc92b31a0a7574eb7f755
lucia.lopuchova <lucia.lopuchova@softpoint.sk>
2023-10-29 22:42:33 +0100

    Edit controller generation and build controller template

--- lib/generators/tag_auth/controller_generator.rb
+++ lib/generators/tag_auth/controller_generator.rb
@@ -5,24 +5,26 @@ module TagAuth
     class ControllerGenerator < Rails::Generators::Base
       source_root File.expand_path('templates', __dir__)
 
-      desc 'Generates a controller responsible for creating one time tokens' \
-      'and user authentication with tokens'
+      desc 'Generates a controller responsible for creating one time sign in tokens' \
+      "based on model's authentication tag"
 
       argument :scope, required: true,
                        desc: 'The scope in which controller will be created, e.g. users.' \
                        'It should be compatible with the model provided in the tag_auth generator'
 
       def create_controller
-        @scope_prefix = scope.blank? ? 'Users' : "#{scope.camelize}::"
+        @model = scope.camelize.singularize
+        @instance = scope.singularize
+        @scope = scope.blank? ? 'Users' : scope.camelize
 
-        template 'tag_auth_tokens_controller.rb.erb',
-                 "app/controllers/#{scope}/tag_auth_tokens_controller.rb"
+        template 'tag_auth_controller.rb.erb',
+                 "app/controllers/#{scope}/tag_auth_controller.rb"
 
       end
 
       def add_routes
-        route "post '/tag_auth/generate_token', to: 'tag_auth_tokens#generate_token'"
-        route "post '/tag_auth/login_with_token', to: 'tag_auth_tokens#login_with_token'"
+        route "get '/tag_auth/sign_in', to: 'tag_auth#new'"
+        route "post '/tag_auth/sign_in', to: 'tag_auth#create'"
       end
     end
   end
