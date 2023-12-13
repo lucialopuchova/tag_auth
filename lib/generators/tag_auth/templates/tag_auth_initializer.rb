ebbc4eb63adf3ceb64e8929b8cdc340f3d39ceed
lucia.lopuchova <lucia.lopuchova@softpoint.sk>
2023-12-13 22:33:59 +0100

    Add commentary for the key generation in initializer + fix space

--- lib/generators/tag_auth/templates/tag_auth_initializer.rb
+++ lib/generators/tag_auth/templates/tag_auth_initializer.rb
@@ -3,6 +3,9 @@ require 'openssl'
 Rails.application.config.tag_auth_encryption_algorithm = 'AES-256-CBC'
 
 # TODO: change for ENV stored key if you are running multiple instances of your app because of load balancing
+# Generate the key using the OpenSSL::Cipher.new with your chosen algorithm and use Base64.strict_encode64(your_key) to
+# store it in ENV. Then uncomment the row below and delete the existing initialization of the tag_auth_encryption_key.
+# Rails.application.config.tag_auth_encryption_key = Base64.strict_decode64(ENV['TAG_AUTH_KEY'])
 Rails.application.config.tag_auth_encryption_key = OpenSSL::Cipher.new(Rails.application.config.tag_auth_encryption_algorithm).random_key
 
 TagAuth.configure do |config|
